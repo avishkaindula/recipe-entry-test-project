59260bd92864b3ca8f4bf96f012c3c8e9d4a6272
Avishka Indula <avishkainduladev@gmail.com>
2023-01-04 16:54:17 +0530

    configured the mongodb atlas database

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("This is our starting app");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://avishka_indula:p7iGGaREtxbhN3t3@cluster0.oayvkaz.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
